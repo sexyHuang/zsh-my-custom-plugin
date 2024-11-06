db146a02f88679bcf7b7d29cb47edf49434b7e1c
Huangjianyong <353016313@qq.com>
2024-11-06 17:58:15 +0800

    fix: sCRIPT_DIR

--- zsh-my-custom.plugin.zsh
+++ zsh-my-custom.plugin.zsh
@@ -1,11 +1,18 @@
 # zsh-my-custom
+
+# 获取当前脚本的目录路径
+SCRIPT_DIR=$(dirname "${0:A}")
+
+# 为脚本添加执行权限
+chmod +x "$SCRIPT_DIR/scripts/create_branch.sh"
+
 chmod +x ./scripts/create_branch.sh
 # 配置项
 export GIT_BRANCH_NAME="huangjianyong"
 export GIT_SOURCE_BRANCH="main"
 
 # 别名配置
-alias gcbr="./scripts/create_branch.sh"
+alias gcbr="$SCRIPT_DIR/scripts/create_branch.sh"
 alias gcbf="gcbr feature"
 alias gcbhf="gcbr hotfix"
 alias gcbfix="gcbr fix"
